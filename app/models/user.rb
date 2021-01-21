a730ebff5d0a2265186e80baf2592371a3db1cfe
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2021-01-21 18:10:59 +0900

    passwordのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
   validates :nickname, presence: true,uniquness: {case_sensitive: true}
 
