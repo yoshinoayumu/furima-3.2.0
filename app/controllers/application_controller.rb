99309876754efd221e761e70d4caedd55d4d29ab
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2021-01-21 17:56:55 +0900

    ユーザー管理機能
    pathの設定
    ストロングパラメーター設定
    viewの設定
    ログインの有無で表示が変わる条件分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, :configure_permitted_parameters, if: :devise_controller?
 
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
