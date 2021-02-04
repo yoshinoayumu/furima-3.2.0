aa41270bb8e5302c458e073f343ca8b5914345b4
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2021-02-04 21:24:56 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    end
+    render 'new'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :price, :scheduled_delivery_id, :shipping_fee_status_id, :prefecture_id, :sales_status_id, :category_id).merge(user_id: current_user.id)
+    
   end
 end
